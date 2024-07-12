1e426ea9d4a5b6bc4e511f8151da79f6f6c517c6
Rohan Shrivastava <rohansh0808@gmail.com>
2024-07-12 18:01:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import TicTacToe from './Components/Tic Tac Toe/TicTacToe'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
